0ff0dc5262a7f64e123778856f288d1c79cba5bd
IgorMeneghini <igormeneghini@outlook.com>
2024-04-12 15:41:06 -0300

    Update: implementation of the second item from TP - Implementation of an algorithm that finds all the articulations in the graph

--- tp-01/Graph.py
+++ tp-01/Graph.py
@@ -1,24 +1,25 @@
 import random
 
+
 class graph:
-    def __init__(self, number_of_vertex) -> None:
-        self.number_of_vertex = number_of_vertex
+    def __init__(self, number_of_vertices) -> None:
+        self.number_of_vertices = number_of_vertices
         self.adjacency_list = self._start_list()
 
     def _start_list(self):
         adjacency_list = {}
-        for i in range(self.number_of_vertex):
+        for i in range(self.number_of_vertices):
             adjacency_list[i] = []
         return adjacency_list
         # this for generate a list of lists going from 0 to 99 empty
 
     def generate_edges(self):
-        for i in range(self.number_of_vertex):
+        for i in range(self.number_of_vertices):
             # this for generate a random number to be the degree of the vertex
-            random_vertex_degree = random.randrange(0, self.number_of_vertex - 1)
+            random_vertex_degree = random.randrange(0, self.number_of_vertices - 1)
             while len(self.adjacency_list[i]) < random_vertex_degree:
                 # this is a loop inside the vertex i to generate the vertices adjacent to i
-                random_vertex = random.randrange(0, self.number_of_vertex)
+                random_vertex = random.randrange(0, self.number_of_vertices)
 
                 if random_vertex not in self.adjacency_list[i]:
                     self.insertion_sort(self.adjacency_list[i], random_vertex)
@@ -36,18 +37,38 @@ class graph:
                 list.append(vertex)
 
     def depth_first_search(self):
-        visited = [False] * self.number_of_vertex
-        print(self.dfs(10, 3, visited))
-        print(self.dfs(10, 3, visited))
+        visited = [False] * self.number_of_vertices
+        print(self.dfs(0, 99, visited))
+        print(self.dfs(0, 99, visited))
 
     def dfs(self, vertex, end, visited):
         print("{} ->".format(vertex), end="")
         if vertex == end:
             return True
         visited[vertex] = True
+        for neighbor in self.adjacency_list[vertex]:
+            if not visited[neighbor]:
+                if self.dfs(neighbor, end, visited):
+                    return True
+        return False
+
+    def find_articulations(self):
+        articulations = []
+        for i in range(len(self.adjacency_list)):
+            visited = [False] * (self.number_of_vertices)
+            self.dfs_articulations(visited, i, 0)
+            for j in range(len(self.adjacency_list)):
+                if j != i:
+                    if not visited[j]:
+                        articulations.append(i)
+                        break
+        return articulations
+
+    def dfs_articulations(self, visited, removed_vertex, vertex=0):
+        visited[vertex] = True
         for i in self.adjacency_list[vertex]:
-            if not visited[i]:
-                return self.dfs(i, end, visited)
+            if not visited[i] and i != removed_vertex:
+                self.dfs_articulations(visited, removed_vertex, i)
 
 
 graph_instance = graph(100)
@@ -55,3 +76,5 @@ graph_instance.generate_edges()
 for i in graph_instance.adjacency_list:
     print("{}: {}".format(i, graph_instance.adjacency_list[i]))
 graph_instance.depth_first_search()
+articulations = graph_instance.find_articulations()
+print(articulations)
